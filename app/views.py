c5535e2aca0ff16f20842c8022987129e4400ebf
Ramazan Emre Erkan <remre98@gmail.com>
2021-06-01 00:33:15 +0300

    explore.html

--- app/views.py
+++ app/views.py
@@ -43,6 +43,7 @@ def explore(request):
         return render(request=request, template_name='explore.html', context=context)
     else:
         posts = Post.objects.all()
+        print(posts)
         context = {
             'user': user,
             'posts': posts
