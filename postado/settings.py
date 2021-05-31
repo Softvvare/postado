ad0b6fa4052588315d0f0b80b712f8fbe3a2d4d3
Ramazan Emre Erkan <remre98@gmail.com>
2021-05-31 23:21:51 +0300

    taggit

--- postado/settings.py
+++ postado/settings.py
@@ -63,26 +63,26 @@ TEMPLATES = [
 WSGI_APPLICATION = 'postado.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'postado_database',
-#         'USER': 'postgres',
-#         'PASSWORD': 'gWH22k!Yt$VC',
-#         'HOST': '159.89.54.81',
-#         'PORT': '5432',
-#     }
-# }
+Database
+https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postado_database',
+        'USER': 'postgres',
+        'PASSWORD': 'gWH22k!Yt$VC',
+        'HOST': '159.89.54.81',
+        'PORT': '5432',
+    }
 }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 
 
 # Password validation
